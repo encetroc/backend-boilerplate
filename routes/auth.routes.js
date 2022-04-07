c1f6f25e1b8ed7ac37acfd5a7e279ad2ddc70eaa
elmehdi rhindi <g6handi@gmail.com>
2022-04-07 20:42:56 +0200

    express validator

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,53 +1,68 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { body } = require("express-validator");
 const User = require("../models/User.model");
+const validate = require("../middlewares/validate.middleware");
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  try {
-    const passwordHash = await bcrypt.hash(password, 10);
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: passwordHash,
-    });
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error);
+router.post(
+  "/signup",
+  validate([
+    body("firstName").isLength({ min: 2 }),
+    body("lastName").isLength({ min: 2 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+  ]),
+  async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    try {
+      const passwordHash = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: passwordHash,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
-});
+);
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      const passwordCorrect = await bcrypt.compare(password, user.password);
-      if (passwordCorrect) {
-        const payload = {
-          user,
-        };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
-          algorithm: "HS256",
-          expiresIn: "6h",
-        });
-        res.status(200).json({
-          user,
-          token,
-        });
+router.post(
+  "/login",
+  validate([body("email").isEmail(), body("password").isLength({ min: 6 })]),
+  async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (user) {
+        const passwordCorrect = await bcrypt.compare(password, user.password);
+        if (passwordCorrect) {
+          const payload = {
+            user,
+          };
+          const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            algorithm: "HS256",
+            expiresIn: "6h",
+          });
+          res.status(200).json({
+            user,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "Email or password are incorrect" });
+        }
       } else {
         res.status(401).json({ message: "Email or password are incorrect" });
       }
-    } else {
-      res.status(401).json({ message: "Email or password are incorrect" });
+    } catch (error) {
+      res.status(500).json(error);
     }
-  } catch (error) {
-    res.status(500).json(error);
   }
-});
+);
 
 module.exports = router;
