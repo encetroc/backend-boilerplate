fe1a4bca193f7f8c661ce8c691b506ddd2dc31c8
elmehdi rhindi <g6handi@gmail.com>
2022-04-23 09:49:52 +0200

    file upload with cloudinary

---
+++ readme.md
@@ -0,0 +1,3 @@
+1. create a `.env` file and add you variable, you can look at `.env.example` file to see what you need to add
+2. `npm i` to install dependencies
+3. `npm run dev` to run the backend
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const upload = require('./config/cloudstorage')
 const { authenticate } = require("./middlewares/jwt.middleware");
 dotenv.config();
 
@@ -19,5 +20,10 @@ app.use("/auth", authRoutes);
 app.post("/test", authenticate, (req, res) => {
   res.send(req.body.test);
 });
+
+// name of the file is "myFile" you can change it to whatever you want
+app.post('/upload', upload.single('myFile'), (req, res) => {
+  res.json(req.file)
+})
 
 app.listen(process.env.PORT);
